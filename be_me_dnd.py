d66153fce36d3fd707b15c1c0f400d2acc06a054
curtistarr <ctarr901@gmail.com>
2019-09-29 12:53:55 +0100

    Formatting

--- be_me_dnd.py
+++ be_me_dnd.py
@@ -3,11 +3,15 @@ import random
 D6_LOWER = 1
 D6_UPPER = 6
 STAT_ROLLS = 4
-STATS = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
-CLASSES = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
-RACES = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tiefling"]
+STATS = ["Strength", "Dexterity", "Constitution",
+         "Intelligence", "Wisdom", "Charisma"]
+CLASSES = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk",
+           "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"]
+RACES = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling",
+         "Half-Orc", "Human", "Tiefling"]
 FIRST_NAMES = "first-names"
 LAST_NAMES = "last-names"
+
 
 def roll_stat():
     rolls = []
@@ -16,17 +20,21 @@ def roll_stat():
     rolls.sort(reverse=1)
     return rolls[1] + rolls[2] + rolls[3]
 
+
 def roll_stats():
     stats = {}
     for stat in STATS:
         stats[stat] = roll_stat()
     return stats
 
+
 def gen_race():
     return RACES[random.randint(0, len(RACES) - 1)]
 
+
 def gen_class():
     return CLASSES[random.randint(0, len(CLASSES) - 1)]
+
 
 def get_random_name_from_file(filename: str):
     names = []
@@ -35,11 +43,14 @@ def get_random_name_from_file(filename: str):
             names.append(name)
     return names[random.randint(0, len(names) - 1)].strip().capitalize()
 
+
 def gen_name():
-    return get_random_name_from_file(FIRST_NAMES) + " " + get_random_name_from_file(LAST_NAMES)
+    return (get_random_name_from_file(FIRST_NAMES) + " "
+            + get_random_name_from_file(LAST_NAMES))
+
 
 if __name__ == "__main__":
     print("> Be me %s" % gen_name())
     print("> %s %s" % (gen_race(), gen_class()))
     for stat, value in roll_stats().items():
-        print ("> %s: %d" % (stat, value))
+        print("> %s: %d" % (stat, value))
